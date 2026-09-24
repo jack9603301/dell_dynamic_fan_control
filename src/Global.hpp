1493f4a477a654cc230c33e0049d17eb688c8f6a
Chunhui Ouyang <jack9603301@qhjack.top>
2026-09-25 01:54:41 +0800

    Version number increment
    Version number increment to v2.2
    
    Signed-off-by: Chunhui Ouyang <jack9603301@qhjack.top>

--- src/Global.hpp
+++ src/Global.hpp
@@ -3,7 +3,7 @@
 
 #define TAG                     "DynamicFanControl"
 #define MAJOR_VERSION           2
-#define MINOR_VERSION           1
+#define MINOR_VERSION           2
 
 #ifdef USE_CMAKE_GENERATED
     #include "version_generated.hpp"
